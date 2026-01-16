8dd0000727ca34bf5fc54dc6f479fa2515641bca
RivadonS <rivadon.sung@bumail.net>
2026-01-16 16:42:47 +0700

    Week1_16012026

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    int _number = 0;
+    string _player = "Rei";
+    string _grade = "A";
+    bool _isPlayer = false;
+    public float _speed = .01f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        Debug.Log(_player);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Transform t = gameObject.GetComponent<Transform>();
+        //t.Translate(0,0, _speed * Time.deltaTime);
+        t.Translate(_speed * Time.deltaTime* Vector3.forward);
 
+        float inputZ = Input.GetAxis("Vertical");
+
+        //Vector3 movement = Vector3.forward * inputZ * _speed * Time.deltaTime;
+
+        //transform.Translate(movement);
     }
 }
